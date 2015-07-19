28a9cb192f50a2d2997c8303a74c19bd2da190e9
Karan Mehta <appllefanetickz@gmail.com>
2015-07-19 05:15:03 -0700

    Commit Search Screen

--- ParseSwiftStarterProject/ParseStarterProject/LoginViewController.swift
+++ ParseSwiftStarterProject/ParseStarterProject/LoginViewController.swift
@@ -27,7 +27,7 @@ class LoginViewController: UIViewController {
             if user != nil {
                 
                 ProgressHUD.showSuccess("Login is Valid!")
-                let main = UIStoryboard(name: "ExploreTStoryBoard", bundle: nil)
+                let main = UIStoryboard(name: "TeamPeople", bundle: nil)
                 let vc = main.instantiateViewControllerWithIdentifier("NavCon") as! UINavigationController
                 self.showViewController(vc, sender: self)
                 //self.performSegueWithIdentifier("logingood", sender: nil)
---
+++ ParseSwiftStarterProject/SearchCandidateCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SearchCandidateCollectionViewCell.swift
+//  Kiwi
+//
+//  Created by Karan Mehta on 7/19/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class SearchCandidateCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var skills: UILabel!
+    @IBOutlet var name: UILabel!
+    @IBOutlet var profilePic: UIImageView!
+}
